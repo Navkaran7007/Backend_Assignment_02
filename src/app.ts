91ff884dccca809b567ea6ae4bb54cf12ad3b707
Navkaran7007 <nsidhu10@rrc.ca>
2025-09-24 13:47:32 -0500

    Integrated Morgan middleware for http logging

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import express, { Express } from "express";
 // Initialize Express application
 const app: Express = express();
 
+// Importing morgan
+import morgan from "morgan";
+
+// Use morgan for HTTP request logging
+app.use(morgan("combined"));
+
 // Define a route
 // @ts-ignore
 app.get("/", (req, res) => {
