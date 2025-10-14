fc95b17c504f723e32a5fe0b10fc472efbfb70ec
Navkaran7007 <nsidhu10@rrc.ca>
2025-10-13 22:27:47 -0500

    Created employee validation schema for create, update, branchparam and departmentparam.

---
+++ src/api/v1/validation/employeeSchemas.ts
@@ -0,0 +1,24 @@
+import Joi from "joi";
+import { EmployeeRequestModel } from "../models/employeeRequestModel";
+
+export const createEmployeeSchema = Joi.object<EmployeeRequestModel>({
+  name: Joi.string().trim().required(),
+  position: Joi.string().trim().required(),
+  email: Joi.string().trim().email({ tlds: { allow: false } }).required(),
+  branchId: Joi.number().optional(),
+});
+
+export const updateEmployeeSchema = Joi.object<EmployeeRequestModel>({
+  name: Joi.string().trim(),
+  position: Joi.string().trim(),
+  email: Joi.string().trim().email({ tlds: { allow: false } }),
+  branchId: Joi.number(),
+}).min(1);
+
+export const branchParamSchema = Joi.object({
+  branchId: Joi.number().required(),
+});
+
+export const departmentParamSchema = Joi.object({
+  department: Joi.string().trim().min(1).required(),
+});
